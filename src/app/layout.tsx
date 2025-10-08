ece21caaa32403fa3a673963a3a7672caf72785b
Mr.Sassas <superluis1800@gmail.com>
2025-10-07 23:25:39 -0600

    se diseño frontend del chatbot con un avance del 50%

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Material Symbols */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
